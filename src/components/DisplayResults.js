641b967da93a58370423d0cf03009f3214b35e6e
npittigl <npittigl@gmail.com>
2023-05-29 14:13:06 -0400

    added js to randomize quotes displayed & for loop to render specified number of quotes

--- src/components/DisplayResults.js
+++ src/components/DisplayResults.js
@@ -11,8 +11,10 @@ function DisplayResults() {
     // initialized state to store topic value as selected by user
     const [quoteTopic, setQuoteTopic] =  useState(null);
 
-    // created state to store quotes objects coming from Quotable API based on user's topic selection
+    // created state to store objects coming from Quotable API based on user's topic selection
     const [quotes, setQuotes] = useState([]);
+
+    const numQuotesToGenerate = 1;
 
     // define side effect that makes call to the Quotable API 
     useEffect(() => {
@@ -25,11 +27,33 @@ function DisplayResults() {
                 params: {
                     // inject topic state into API call; as state changes, the API call (as dictated by dependency array) will run when form submitted
                     query: quoteTopic,
-                    limit: 2
+                    limit: 100
                 }
             }).then((apiData) => {
-                // update state using the array returned to us from the API:
-                setQuotes(apiData.data.results);
+                // store api data in variable
+                const quotesArray = apiData.data.results;
+
+                // filter out quotes that exceed 120 characters
+                const filteredQuotesArray = quotesArray.filter((quoteItem) => {
+                    return quoteItem.length <= 120;
+                });
+
+                // variable to store random quote(s)
+                const randomizedQuotesArray = [];
+
+                // function to push random quote from filtered array into new array
+                function randomQuotes(randomNumber) {
+                    randomizedQuotesArray.push(filteredQuotesArray[randomNumber]);
+                }
+
+                // for loop to generate random index number and call randomQuote() function
+                for(let i=0; i<numQuotesToGenerate; i++) {
+                    let randomImageIndex = Math.floor(Math.random() * filteredQuotesArray.length);
+                    randomQuotes(randomImageIndex);
+                }
+
+                // update state with array of randomized quotes
+                setQuotes(randomizedQuotesArray);
             });
         }
         // if quoteTopic is not equal to null, then call getApiData function
@@ -40,12 +64,12 @@ function DisplayResults() {
         // use topic state inside dependency array, so every time state changes, the side effect (API call), runs and returns quotes based on user's chosen topic
     }, [quoteTopic]);
 
-    // 4. Define an event handler; passed to Form component via props (Form will trigger this event handler function when the form is submitted)
+    // event handler to set user's topic choice & trigger useEffect upon form submission (passed to Form component via props)
     const handleSubmit = (event, usersChoice) => {
         // prevent page reload on submit
         event.preventDefault();
 
-        // update topic state with the user's choice
+        // update topic state with the user's choice; if user submits before topic is chosen, user gets alert 
         if (usersChoice !== "Choose One:") {
             setQuoteTopic(usersChoice);
         } else {
