b4169f2f75b3b95f29538a51b046c1c757628a3d
npittigl <npittigl@gmail.com>
2023-07-15 12:42:54 -0400

    updated form select/options to be more accessible

--- src/components/optionsArray.js
+++ src/components/optionsArray.js
@@ -1,7 +1,7 @@
 
 // array of options for users to select from; imported in Form component
 const optionsArray = [
-    {query: 'change, life', topicSelection: 'Change'},
+    { query: 'change, life', topicSelection: 'Change'},
     { query: 'happy, happiness, joy', topicSelection: 'Happiness' },
     { query: 'life, work', topicSelection: 'Life' },
     { query: 'love, loving', topicSelection: 'Love' },
